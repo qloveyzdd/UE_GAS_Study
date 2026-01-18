19e22b57adf3949954112d604a1e879f68bb57ab
qloveyzdd <62373999+qloveyzdd@users.noreply.github.com>
2026-01-19 00:33:08 +0800

    编写生成碰撞盒子的动画通知

---
+++ Source/UE_GAS_Study/Notify/AnimNotify_Attack.cpp
@@ -0,0 +1,58 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AnimNotify_Attack.h"
+
+#include "UE_GAS_Study/DamagedActor/UE_GAS_StudyHitBox.h"
+
+UAnimNotify_Attack::UAnimNotify_Attack()
+{
+	HitObjectClass = AUE_GAS_StudyHitBox::StaticClass();
+	BoxExtent = FVector(32.0f);
+	LifeTime = 4.0f;
+
+	InSocketName = TEXT("OpenFire");
+}
+
+FString UAnimNotify_Attack::GetNotifyName_Implementation() const
+{
+	return Super::GetNotifyName_Implementation();
+}
+
+void UAnimNotify_Attack::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+                                const FAnimNotifyEventReference& EventReference)
+{
+	Super::Notify(MeshComp, Animation, EventReference);
+
+	if (AActor* InCharacter = Cast<AActor>(MeshComp->GetOuter()))
+	{
+		FVector ComponentLocation = MeshComp->GetSocketLocation(InSocketName);
+		FRotator ComponentRotation = MeshComp->GetSocketRotation(InSocketName);
+
+		if (!InCharacter->GetWorld()->IsNetMode(NM_Client))
+		{
+			FTransform Transform((ComponentRotation + RotationOffset).Quaternion(), ComponentLocation);
+
+			//延迟生成
+			if (AUE_GAS_StudyHitBox* HitCollision = InCharacter->GetWorld()->SpawnActorDeferred<AUE_GAS_StudyHitBox>(
+				HitObjectClass, Transform,NULL, Cast<APawn>(InCharacter),
+				ESpawnActorCollisionHandlingMethod::AlwaysSpawn))
+			{
+				HitCollision->SetBuffs(BuffTags);
+				HitCollision->SetBoxExtent(BoxExtent);
+
+				FVector RelativeLocation = HitCollision->GetHitDamage()->GetRelativeLocation();
+				HitCollision->SetHitDamageRelativePosition(RelativeLocation + RelativeOffsetLocation);
+				
+				HitCollision->SetLifeSpan(LifeTime);
+				
+				HitCollision->FinishSpawning(Transform);
+
+				if (bBind)
+				{
+					HitCollision->AttachToComponent(MeshComp,FAttachmentTransformRules::SnapToTargetIncludingScale,InSocketName);
+				}
+			}
+		}
+	}
+}
---
+++ Source/UE_GAS_Study/Notify/AnimNotify_Attack.h
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "AnimNotify_Attack.generated.h"
+
+/**
+ * 
+ */
+UCLASS(meta = (DisplayName = "Attack"))
+class UE_GAS_STUDY_API UAnimNotify_Attack : public UAnimNotify
+{
+	GENERATED_BODY()
+
+public:
+	UAnimNotify_Attack();
+
+	virtual FString GetNotifyName_Implementation() const override;
+
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+	                    const FAnimNotifyEventReference& EventReference) override;
+	
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="General Configuration")
+	TArray<struct FGameplayTagContainer> BuffTags;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="General Configuration")
+	TSubclassOf<class AUE_GAS_StudyHitBox> HitObjectClass;
+	
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="General Configuration")
+	FVector RelativeOffsetLocation;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="General Configuration")
+	FRotator RotationOffset;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="General Configuration")
+	float LifeTime;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="General Configuration")
+	FName InSocketName;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="General Configuration")
+	bool bBind;
+	
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Hit Box")
+	FVector BoxExtent;
+};

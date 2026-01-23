14a68ca3abaee31b4e9988439b9937830dcd5562
qloveyzdd <62373999+qloveyzdd@users.noreply.github.com>
2026-01-24 00:50:27 +0800

    冲刺技能

---
+++ Source/UE_GAS_Study/NotifyState/AnimNotifyState_AddForce.cpp
@@ -0,0 +1,65 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AnimNotifyState_AddForce.h"
+
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+FVector UAnimNotifyState_AddForce::GetCurrentCharacterDirection(ACharacter* InCharacter)
+{
+	FVector V = FVector::ZeroVector;
+
+	if (InCharacter)
+	{
+		if (DirectionForce.X != 0.0f)
+		{
+			V += InCharacter->GetActorForwardVector() * DirectionForce.X;
+		}
+		else if (DirectionForce.Y != 0.0f)
+		{
+			V += InCharacter->GetActorRightVector() * DirectionForce.Y;
+		}
+		else if (DirectionForce.Z != 0.0f)
+		{
+			V += InCharacter->GetActorUpVector() * DirectionForce.Z;
+		}
+	}
+
+	return V;
+}
+
+UAnimNotifyState_AddForce::UAnimNotifyState_AddForce()
+	: Super(), TotalDurationConsuming(0.0f), ForceSizeConsuming(0.0f),
+	  DirectionForce(1.0f, 0.0f, 0.0f), ForceSize(180000.0f)
+{
+}
+
+void UAnimNotifyState_AddForce::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+                                            float TotalDuration, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyBegin(MeshComp, Animation, TotalDuration, EventReference);
+
+	if (ACharacter* InCharacter = Cast<ACharacter>(MeshComp->GetOuter()))
+	{
+		FVector NewFirection = GetCurrentCharacterDirection(InCharacter);
+		InCharacter->GetCharacterMovement()->AddImpulse(ForceSize * NewFirection, true);
+	}
+}
+
+void UAnimNotifyState_AddForce::NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+                                           float FrameDeltaTime, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyTick(MeshComp, Animation, FrameDeltaTime, EventReference);
+}
+
+void UAnimNotifyState_AddForce::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+                                          const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyEnd(MeshComp, Animation, EventReference);
+
+	if (ACharacter* InCharacter = Cast<ACharacter>(MeshComp->GetOuter()))
+	{
+		InCharacter->GetCharacterMovement()->StopMovementImmediately();
+	}
+}
---
+++ Source/UE_GAS_Study/NotifyState/AnimNotifyState_AddForce.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "AnimNotifyState_AddForce.generated.h"
+
+/**
+ * 
+ */
+UCLASS(meta = (DisplayName = "AddForce"))
+class UE_GAS_STUDY_API UAnimNotifyState_AddForce : public UAnimNotifyState
+{
+	GENERATED_BODY()
+
+private:
+	float TotalDurationConsuming;
+
+	float ForceSizeConsuming;
+
+	FVector GetCurrentCharacterDirection(ACharacter* InCharacter);
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="AnimNotifyState_AddForce")
+	FVector DirectionForce;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="AnimNotifyState_AddForce")
+	float ForceSize;
+
+public:
+	UAnimNotifyState_AddForce();
+
+	virtual void NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration,
+	                         const FAnimNotifyEventReference& EventReference) override;
+	virtual void NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime,
+	                        const FAnimNotifyEventReference& EventReference) override;
+	virtual void NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+	                       const FAnimNotifyEventReference& EventReference) override;
+};

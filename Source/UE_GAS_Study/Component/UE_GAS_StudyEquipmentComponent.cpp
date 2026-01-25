831057981e268d28b420706f9453b8fdd9798d2d
qloveyzdd <62373999+qloveyzdd@users.noreply.github.com>
2026-01-26 00:10:14 +0800

    编写装备组件基本逻辑

--- Source/UE_GAS_Study/Component/UE_GAS_StudyEquipmentComponent.cpp
+++ Source/UE_GAS_Study/Component/UE_GAS_StudyEquipmentComponent.cpp
@@ -3,13 +3,41 @@
 
 #include "UE_GAS_StudyEquipmentComponent.h"
 
+#include "UE_GAS_StudyInventoryComponent.h"
+#include "Net/UnrealNetwork.h"
+#include "UE_GAS_Study/AbilitySystem/UE_GAS_StudyAbilitySystemComponent.h"
+#include "UE_GAS_Study/Character/UE_GAS_StudyCharacterBase.h"
+#include "UE_GAS_Study/Item/UE_GAS_StudyEquipment.h"
+
+
+FUE_GAS_StudyEquipmentItem::FUE_GAS_StudyEquipmentItem()
+	: RPGEquipmentItemPoint(nullptr)
+{
+}
+
+bool FUE_GAS_StudyEquipmentItem::IsValid() const
+{
+	if (RPGEquipmentItemPoint)
+	{
+		return true;
+	}
+	return false;
+}
+
+void FUE_GAS_StudyEquipmentItem::ResetSelf()
+{
+	ActiveEquipmentEffectHandle.Invalidate();
+
+	RPGEquipmentItemPoint = nullptr;
+}
 
 // Sets default values for this component's properties
-UUE_GAS_StudyEquipmentComponent::UUE_GAS_StudyEquipmentComponent()
+UUE_GAS_StudyEquipmentComponent::UUE_GAS_StudyEquipmentComponent(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = false;
 
 	// ...
 }
@@ -21,16 +49,124 @@ void UUE_GAS_StudyEquipmentComponent::BeginPlay()
 	Super::BeginPlay();
 
 	// ...
-	
 }
 
-
-// Called every frame
-void UUE_GAS_StudyEquipmentComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+void UUE_GAS_StudyEquipmentComponent::TickComponent(float DeltaTime, enum ELevelTick TickType,
                                                     FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	// ...
 }
 
+bool UUE_GAS_StudyEquipmentComponent::AddEquipmentItem(const FUE_GAS_StudyEquipmentItem& NewItem)
+{
+	UUE_GAS_StudyAbilitySystemComponent* ASC = Cast<AUE_GAS_StudyCharacterBase>(GetOwner())->
+		GetGASStudyAbilitySystemComponent();
+
+	if (HasAuthority())
+	{
+		if (!NewItem.IsValid())
+		{
+			UE_LOG(LogTemp, Warning, TEXT("AddInventoryItem: Invalid Equipment Item"));
+			return false;
+		}
+		for (int i = 0; i < EquipmentItems.Num(); ++i)
+		{
+			if (!EquipmentItems[i].IsValid())
+			{
+				EquipmentItems[i] = NewItem;
+
+				const FGameplayEffectSpecHandle NewHandle = ASC->MakeOutgoingSpec(
+					EquipmentItems[i].RPGEquipmentItemPoint->GameplayEffectClass, 1.0f, ASC->MakeEffectContext());
+				EquipmentItems[i].ActiveEquipmentEffectHandle = ASC->ApplyGameplayEffectSpecToSelf(*NewHandle.Data);
+
+				// EquipmentItemChange(EquipmentItems);
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+void UUE_GAS_StudyEquipmentComponent::UndockEquipmentById(int32 InUndockEquipmentID)
+{
+	if (HasAuthority())
+	{
+		UUE_GAS_StudyInventoryComponent* InventoryComponent = Cast<UUE_GAS_StudyInventoryComponent>(
+			GetOwner()->FindComponentByClass(UUE_GAS_StudyInventoryComponent::StaticClass()));
+
+		if (EquipmentItems[InUndockEquipmentID].IsValid())
+		{
+			FUE_GAS_StudyEquipmentItem* UndockEquipment = &EquipmentItems[InUndockEquipmentID];
+
+			FUE_GAS_StudyInventoryItem AddInventoryItem;
+			AddInventoryItem.GAS_StudyItem = UndockEquipment->RPGEquipmentItemPoint;
+			AddInventoryItem.ItemCount = 1;
+
+			if (InventoryComponent->AddInventoryItem(AddInventoryItem))
+			{
+				RemoveEquipmentItem(InUndockEquipmentID);
+			}
+		}
+	}
+}
+
+bool UUE_GAS_StudyEquipmentComponent::RemoveEquipmentItem(int32 Index_Remove)
+{
+	UUE_GAS_StudyAbilitySystemComponent* ASC = Cast<AUE_GAS_StudyCharacterBase>(GetOwner())->
+		GetGASStudyAbilitySystemComponent();
+	if (FUE_GAS_StudyEquipmentItem* UndockEquipment = &EquipmentItems[Index_Remove])
+	{
+		if (UndockEquipment->IsValid())
+		{
+			ASC->RemoveActiveGameplayEffect(UndockEquipment->ActiveEquipmentEffectHandle);
+
+			UndockEquipment->ResetSelf();
+
+			// EquipmentItemChange(EquipmentItems);
+			return true;
+		}
+	}
+	return false;
+}
+
+void UUE_GAS_StudyEquipmentComponent::SwapFromInventoryToEquipment(int32 InventoryIndex, int32 EquipmentIndex)
+{
+}
+
+void UUE_GAS_StudyEquipmentComponent::SwapFromEquipmentToInventory(int32 EquipmentIndex, int32 InventoryIndex)
+{
+}
+
+void UUE_GAS_StudyEquipmentComponent::SwapEuipmentItem(int32 Index_i, int32 Index_j)
+{
+	if (Index_i != Index_j
+		&& Index_i > INDEX_NONE && Index_i < EquipmentItems.Num()
+		&& Index_j > INDEX_NONE && Index_j < EquipmentItems.Num())
+	{
+		EquipmentItems.Swap(Index_i, Index_j);
+
+		EquipmentItemChange(EquipmentItems);
+	}
+}
+
+void UUE_GAS_StudyEquipmentComponent::EquipmentItemChange_Implementation(
+	const TArray<FUE_GAS_StudyEquipmentItem>& ClientEquipmentItems)
+{
+	OnEquipmentItemChanged.ExecuteIfBound(EquipmentItems);
+}
+
+void UUE_GAS_StudyEquipmentComponent::GetLifetimeReplicatedProps(
+	TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(UUE_GAS_StudyEquipmentComponent, EquipmentItems);
+}
+
+void UUE_GAS_StudyEquipmentComponent::OnRep_EquipmentItems()
+{
+	OnEquipmentItemChanged.ExecuteIfBound(EquipmentItems);
+
+	EquipmentItems.AddDefaulted(6);
+}
--- Source/UE_GAS_Study/Component/UE_GAS_StudyEquipmentComponent.h
+++ Source/UE_GAS_Study/Component/UE_GAS_StudyEquipmentComponent.h
@@ -3,9 +3,30 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "ActiveGameplayEffectHandle.h"
 #include "UE_GAS_StudyComponentBase.h"
 #include "UE_GAS_StudyEquipmentComponent.generated.h"
 
+USTRUCT(BlueprintType)
+struct FUE_GAS_StudyEquipmentItem
+{
+	GENERATED_BODY()
+
+public:
+	FUE_GAS_StudyEquipmentItem();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="UE_GAS_StudyEquipmentItem")
+	class UUE_GAS_StudyEquipment* RPGEquipmentItemPoint;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="UE_GAS_StudyEquipmentItem")
+	FActiveGameplayEffectHandle ActiveEquipmentEffectHandle;
+
+	bool IsValid() const;
+
+	void ResetSelf();
+};
+
+DECLARE_DELEGATE_OneParam(FOnEquipmentItemChangedNative, const TArray<FUE_GAS_StudyEquipmentItem>&);
 
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class UE_GAS_STUDY_API UUE_GAS_StudyEquipmentComponent : public UUE_GAS_StudyComponentBase
@@ -14,14 +35,47 @@ class UE_GAS_STUDY_API UUE_GAS_StudyEquipmentComponent : public UUE_GAS_StudyCom
 
 public:
 	// Sets default values for this component's properties
-	UUE_GAS_StudyEquipmentComponent();
+	UUE_GAS_StudyEquipmentComponent(const FObjectInitializer& ObjectInitializer);
 
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
 public:
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType,
 	                           FActorComponentTickFunction* ThisTickFunction) override;
+	
+public:
+	UFUNCTION(Client, Reliable)
+	void EquipmentItemChange(const TArray<FUE_GAS_StudyEquipmentItem>& ClientEquipmentItems);
+	
+public:
+	UFUNCTION(BlueprintCallable, Category="Equipment")
+	bool AddEquipmentItem(const FUE_GAS_StudyEquipmentItem& NewItem);
+	
+	UFUNCTION(BlueprintCallable, Category="Equipment")
+	void UndockEquipmentById(int32 InUndockEquipmentID);
+	
+	UFUNCTION(BlueprintCallable, Category="Equipment")
+	bool RemoveEquipmentItem(int32 Index_Remove);
+	
+public:
+	UFUNCTION(BlueprintCallable, Category="Equipment")
+	void SwapEuipmentItem(int32 Index_i, int32 Index_j);
+	UFUNCTION(BlueprintCallable, Category="Equipment")
+	void SwapFromInventoryToEquipment(int32 InventoryIndex, int32 EquipmentIndex);
+	UFUNCTION(BlueprintCallable, Category="Equipment")
+	void SwapFromEquipmentToInventory(int32 EquipmentIndex, int32 InventoryIndex);
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, ReplicatedUsing= OnRep_EquipmentItems, Category="Equipment")
+	TArray<FUE_GAS_StudyEquipmentItem> EquipmentItems;
+
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+
+public:
+	FOnEquipmentItemChangedNative OnEquipmentItemChanged;
+
+	UFUNCTION()
+	void OnRep_EquipmentItems();
 };
